3b4199176b4aa031636a629ecfbcab3501040a59
Pablo Terradillos <tehsis@gmail.com>
2015-08-09 21:53:08 -0300

    Moved normalizer code to constructor

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import ID from 'incremental-dom';
 
 // TODO: Complete
 const TAGS = ['div', 'span', 'ul', 'li', 
-              'form', 'input', 'button', 'i', 
+              'form', 'button', 'i', 
               'a', 'h1', 'h2', 'h3', 
               'h4', 'h5', 'table', 'td', 'tr',
               'tbody', 'thead'
               ];
 
+const SELF_CLOSING_TAGS = ['input', 'img'];
 
 /**
  * Represents a HTML Element
@@ -16,22 +17,30 @@ export class EL {
   /**
    * Creates a new element
    *
-   * @abstract
    * @param {string} tag the specific HTML TAG of the Element.
    * @param {null|String|EL|Array} content
    * @param {Array} props
    */
   constructor(tag, content=null, key='', props=[]) {
     this.tag = tag;
+
     this.content = content;
+
+    // this allows content to be a string o a single element.
+    if ('string' === typeof this.content) {
+      this.content = new TEXT(this.content);
+    }
+
+    if (!Array.isArray(this.content)) {
+      this.content = [this.content];
+    }
+
     this.key = key;
     this.props = props;
   }
 
   /**
-   * Renders this Element.
-   *
-   *
+   * renders this element and its children.
    */
   render() {
     if (null === this.content) {
@@ -39,23 +48,18 @@ export class EL {
       return;
     }
 
-    if (!Array.isArray(this.content)) {
-      this.content = [this.content];
-    }
-
     ID.elementOpen(this.tag, this.key, this.props);
 
-    this.content.forEach((c) => {
-      if ('string' === typeof c) {
-        c = new TEXT(c);
-      }
-
-      c.render();
-    });
+    this.content.forEach((c) => c.render());
 
     ID.elementClose(this.tag);
   }
 
+  /**
+   * Render element into DOM
+   *
+   * @params {HTMLElement} container the container where the elements is going to be patched.
+   */
   renderTo(container) {
     ID.patch(container, this.render.bind(this));
   }
@@ -80,8 +84,15 @@ let pub = TAGS.reduce((prev, tag) => {
   return prev;
 }, {});
 
+SELF_CLOSING_TAGS.reduce((prev, tag) => {
+  prev[tag] = (key, props) => {
+    return new EL(tag, null, key, props);
+  }
 
-// Generic Element constructor
+  return prev;
+}, pub);
+
+// Generic Element builder
 pub.el = (tag, elms, key, props) => new EL(tag, elms, key, props);
 
 export default pub;
